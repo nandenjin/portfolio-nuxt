0dce0ce9670443c013c596c9272870c302ef80f9
Kazumi Inada <timer.ground.gm@gmail.com>
2018-06-22 18:14:49 +0900

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+
+<template>
+
+  <header></header>
+  
+  <nuxt></nuxt>
+
+</template>
+
+<style lang="scss" scoped></style>
+
+<script>
+
+  export default {};
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+
+<template>
+  
+  <div>Coming soon.</div>
+
+</template>
+
+<style lang="scss" scoped></style>
+
+<sciprt>
+  
+  export default {
+
+
+
+  };
+
+</sciprt>
