1e0c92b0855eec2c3e2654b7cd076849416f4938
Kazumi Inada <hello@nandenjin.com>
2019-06-14 12:32:19 +0900

    Add error page

---
+++ src/layouts/error.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <main>
+    <h1 v-if="error.statusCode === 404">
+      ページが見つかりません
+    </h1>
+    <h1 v-else>
+      エラーが発生しました
+    </h1>
+  </main>
+</template>
+
+<style lang="sass" scoped>
+
+  @import '~/assets/style/media.sass'
+
+</style>
+
+<script lang="ts">
+  import { Component, Vue, Prop } from 'vue-property-decorator'
+
+  @Component
+  export default class ErrorPage extends Vue {
+    @Prop({ type: Object, required: true }) error!: Object
+  }
+
+</script>
