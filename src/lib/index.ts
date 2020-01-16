2891a01d00714715903c0d6b9dec2e83fa9027c7
Kazumi Inada <hello@nandenjin.com>
2020-01-16 09:19:55 +0900

    Refactor

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,23 +1,5 @@
-import * as path from 'path'
+import { getPagesByIndex } from './getPagesByIndex'
 
-export async function getPagesByIndex (indexContent: string) {
-  const matchPattern = /<li.*?>.*?<a .*?href=["'](.+?)["'].*?>(.+?)<\/a>.*?<\/li>/ig
-  let matchResult
-  const dataReqs: Promise<any>[] = []
-
-  while ((matchResult = matchPattern.exec(indexContent)) !== null) {
-    const route = matchResult[1]
-    const dir = path.dirname(route)
-    const id = path.basename(route, '.md')
-    const dataReq = import(`~/../tmp/contents/json${dir}/${id}.json`)
-    dataReqs.push(dataReq)
-  }
-
-  const data = await Promise.all(dataReqs)
-  const items: any = []
-  for (let i = 0; i < data.length; i++) {
-    items.push(data[i].default)
-  }
-
-  return items
+export {
+  getPagesByIndex
 }
