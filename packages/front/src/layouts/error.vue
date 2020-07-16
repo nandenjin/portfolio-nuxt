803c8ff479f78701c70049c2d5d82327191e82e7
Kazumi Inada <hello@nandenjin.com>
2020-07-16 15:26:51 +0900

    fix: nuxt-property-decoratorへ置き換え

--- packages/front/src/layouts/error.vue
+++ packages/front/src/layouts/error.vue
@@ -15,7 +15,7 @@
 </style>
 
 <script lang="ts">
-import { Component, Vue, Prop } from 'vue-property-decorator'
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
 import { NuxtError } from '@nuxt/types'
 
 @Component
--- packages/front/src/pages/news/index.vue
+++ packages/front/src/pages/news/index.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component } from 'nuxt-property-decorator'
 import LinkList from '~/components/LinkList.vue'
 import { getPagesByIndex } from '~/lib'
 
