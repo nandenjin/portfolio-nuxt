b8e5f1f03e4aa89c99aebe34ae5cb40c20ac4f2f
Kazumi Inada <hello@nandenjin.com>
2020-08-19 10:32:15 +0900

    feat: ヘッダにソーシャルへのリンクを追加

--- packages/front/src/components/TheHeader.vue
+++ packages/front/src/components/TheHeader.vue
@@ -4,7 +4,7 @@
       <img src="~/assets/img/kazumi_inada.svg" alt="Kazumi Inada" />
     </nuxt-link>
     <nav class="menu">
-      <ul>
+      <ul class="in-site">
         <nuxt-link tag="li" to="/works">
           Works
         </nuxt-link>
@@ -14,6 +14,11 @@
         <nuxt-link tag="li" to="/news">
           News
         </nuxt-link>
+      </ul>
+      <ul class="social">
+        <li class="icon icon--twitter"><a href="https://twitter.com/nandenjin" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+        <li class="icon icon--instagram"><a href="https://instagram.com/nandenjin" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+        <li class="icon icon--github"><a href="https://github.com/nandenjin" target="_blank" rel="noopener noreferrer">GitHub</a></li>
       </ul>
     </nav>
   </header>
@@ -32,9 +37,16 @@ export default class TheHeader extends Vue {
 @import '~/assets/style/media.sass';
 
 .header {
+  display: grid;
+  grid-template-columns: 200px 1fr;
+  grid-gap: 55px;
   max-width: 1000px;
   width: calc(100vw - 30px * 2);
   margin: 50px auto 0;
+
+  @include rmq() {
+    display: block;
+  }
 
   * {
     vertical-align: middle;
@@ -43,30 +55,82 @@ export default class TheHeader extends Vue {
   .logo {
     display: inline-block;
     width: 200px;
-    margin: 30px 40px 30px 0;
+    transform: translateY(7px);
   }
 
   .menu {
-    display: inline-block;
+    display: inline-flex;
 
-    ul {
+    @include rmq() {
+      margin-top: 30px;
+    }
+
+    .in-site, .social {
       list-style-type: none;
       margin: 0;
       padding: 0;
+
+      li {
+        display: inline-block;
+        font-size: 14px;
+        letter-spacing: 0.1em;
+        cursor: pointer;
+      }
     }
 
-    li {
-      display: inline-block;
-      margin-right: 25px;
-      padding: 7px 0px 3px;
-      text-transform: uppercase;
-      font-size: 14px;
-      letter-spacing: 0.1em;
-      border-bottom: 1px solid transparent;
-      cursor: pointer;
+    .in-site {
+      flex: 1 1 auto;
 
-      &.nuxt-link-active {
-        border-bottom: 1px solid #000;
+      li {
+        margin-right: 25px;
+        text-transform: uppercase;
+        padding: 7px 0px 3px;
+        border-bottom: 1px solid transparent;
+
+        &.nuxt-link-active {
+          border-bottom: 1px solid #000;
+        }
+      }
+    }
+
+    .social {
+      flex: 0 0 auto;
+
+      @include rmq() {
+        display: none;
+      }
+
+      li {
+        margin-left: 5px;
+
+        a {
+          position: relative;
+          color:rgba(0, 0, 0, 0);
+          user-select: none;
+          text-indent: -1000vw;
+        }
+      }
+
+      .icon {
+        display: inline-block;
+        width: 26px;
+        height: 26px;
+        background-size: 17px;
+        background-repeat: no-repeat;
+        background-position: center;
+        overflow: hidden;
+      }
+
+      .icon--twitter {
+        background-image: url('~assets/img/social-logo-twitter.svg')
+      }
+
+      .icon--instagram {
+        background-image: url('~assets/img/social-logo-instagram.svg')
+      }
+
+      .icon--github {
+        background-image: url('~assets/img/social-logo-github.svg')
       }
     }
   }
